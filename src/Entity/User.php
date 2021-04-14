6936b67a261d359de2aaa4a62d0baab3b2435277
brunobc88 <73168161+brunobc88@users.noreply.github.com>
2021-04-14 07:35:35 +0200

    Contraints Unique sur pseudo

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,7 +11,8 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
- * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
+ * @UniqueEntity(fields={"email"}, message="Cet email est déjà utilisé, merci d'en choisir un autre")
+ * @UniqueEntity(fields={"pseudo"}, message="Cet pseudo est déjà utilisé, merci d'en choisir un autre")
  */
 class User implements UserInterface
 {
