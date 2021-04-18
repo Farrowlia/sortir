9398661d6f5bdf226726a48397b62f8568516c1a
brunobc88 <73168161+brunobc88@users.noreply.github.com>
2021-04-18 11:03:31 +0200

    Création requete Ajax js partagée

--- public/js/ville.js
+++ public/js/ville.js
@@ -1,36 +1,9 @@
 
 function modifierVille(id) {
-    document.getElementById("afficherNomVille" + id).innerHTML="<input name='nom' id=\"nomVille" + id + "\" type=\"text\" placeholder=\"Ville ...\">";
-    document.getElementById("afficherCPville" + id).innerHTML="<input name='codePostal' id=\"cpVille" + id + "\" type=\"text\" placeholder=\"Code postal ...\">" + "<input type='hidden' name='id' value=" + id + ">";
-    document.getElementById("modifier" + id).innerHTML="<input value='Enregistrer' type='button' onclick='enregistrerVille(" + id + ")'>";
-}
+    document.getElementById("afficherNomVille" + id).innerHTML="<input name='nom' type='text' placeholder='Ville ...'>";
+    document.getElementById("afficherCPville" + id).innerHTML="<input name='codePostal' type='text' placeholder='Code postal ...'>" +
+                                                                        "<input name='id' type='hidden' value=" + id + ">";
+    document.getElementById("modifier" + id).innerHTML="<input type='button' value='Enregistrer'>";
 
-function enregistrerVille(id) {
-
-    const nameId = "#formVille" + id;
-    const formAjax = document.querySelector(nameId);
-
-    const Form = new FormData(formAjax);
-    const Params = new URLSearchParams();
-
-    Form.forEach((value, key) => {
-        Params.append(key, value);
-    });
-
-    const Url = new URL(window.location.href);
-
-    fetch(Url.pathname + "?" + Params.toString() + "&ajax=1", {
-        headers: {
-            "X-Requested-With": "XMLHttpRequest"
-        }
-    }).then(response =>
-        response.json()
-    ).then(data => {
-
-        const content = document.querySelector(nameId);
-        content.innerHTML = data.content;
-
-        history.pushState({}, null, Url.pathname);
-    }).catch(e => alert(e));
-
+    requeteAjaxFormulaire('#formVille' + id, '#formVille' + id + ' input[type="button"]', "click", '#formVille' + id, true);
 }
