ef16d6f64236f4ff6e9b079ab361952293aa1197
Equilibrium2046 <79090637+Equilibrium2046@users.noreply.github.com>
2021-04-22 15:29:44 +0200

    Anne

--- public/js/ville.js
+++ public/js/ville.js
@@ -1,9 +1,15 @@
-function modifierVille(id) {
+function modifierVille(id, xnom, xcodePostal) {
 
-    document.getElementById("afficherNomVille" + id).innerHTML="<input name='nom' type='text' placeholder='Ville ...'>";
-    document.getElementById("afficherCPville" + id).innerHTML="<input name='codePostal' type='text' placeholder='Code postal ...'>" +
+    document.getElementById("afficherNomVille" + id).innerHTML="<input name='nom' type='text' placeholder='" + xnom + "'>";
+    document.getElementById("afficherCPville" + id).innerHTML="<input name='codePostal' type='text' placeholder='" + xcodePostal + "'>" +
                                                                         "<input name='id' type='hidden' value=" + id + ">";
     document.getElementById("modifier" + id).innerHTML="<input class='btn btn-outline-secondary' type='button' value='Enregistrer'>";
+    document.getElementById("delete" + id).innerHTML="<input class='btn btn-outline-secondary' type='button' value='Supprimer'>";
 
     requeteAjaxFormulaire('#formVille' + id, '#formVille' + id + ' input[type="button"]', "click", '#formVille' + id, true);
 }
+
+// <input className='btn btn-outline-secondary' type='button' value='Supprimer'>
+
+// <a className='btn btn-outline-secondary' id='annuler'>Annuler</a>
+// href='{{ path ('villes') }}'
