822bbca872b3420497a55b8b47ec8d6f5c01d678
Equilibrium2046 <79090637+Equilibrium2046@users.noreply.github.com>
2021-04-22 09:25:52 +0200

    Anne
    Campus et utilisateurs ok sauf supprimer

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserFormType;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,6 +11,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 
 class AdminController extends AbstractController
@@ -30,9 +32,8 @@ class AdminController extends AbstractController
         if ($request->get('rechUser')) {
 
             $motRecherche = $request->get('rechUser');
-//            $result = $userRepository->findByMotRecherche((string)array("nom" => $motRecherche));
 
-            $result = $userRepository->findBy(array("nom" => $motRecherche), array("nom" => "ASC"), null, 0);
+            $result = $userRepository->findByMotRecherche($motRecherche);
 
             return new JsonResponse([
                 'content' => $this->renderView('admin/content/_userSearch.html.twig', compact('result'))
@@ -43,37 +44,95 @@ class AdminController extends AbstractController
             'users' => $tableauUsers
         ]);
     }
-//
-//    public function findByMotRecherche(string $motRecherche) {
-//
-//        $queryBuilder = $this->createQueryBuilder('m');
-//        $queryBuilder->add('select', new Expr\Select(array('u')))
-//                        ->add('from', new Expr\From('User', 'u'))
-//                        ->add('where', $queryBuilder->expr()->orX(
-//                            $queryBuilder->expr()->eq('u.nom', '?1'),
-//                            $queryBuilder->expr()->like('u.prenom', '?2'),
-//                            $queryBuilder->expr()->like('u.pseudo', '?3'),
-//                            $queryBuilder->expr()->like('u.email', '?4')
-//                        ));
-//        $query = $queryBuilder->getQuery();
-//        return $query->getResult();
-//
-//    }
 
-//    public function findByMotRecherche(string $motRecherche): ?User {
-//
-//        dd('test');
-//        $queryBuilder = $this->createQueryBuilder('u');
-//        $queryBuilder->add('select', 'u')
-//            ->add('from', 'User')
-//            ->add('where', $queryBuilder->expr()->orX(
-//                $queryBuilder->expr()->eq('u.nom', '?1'),
-//                $queryBuilder->expr()->like('u.prenom', '?2'),
-//                $queryBuilder->expr()->like('u.pseudo', '?3'),
-//                $queryBuilder->expr()->like('u.email', '?4')
-//            ));
-//        $query = $queryBuilder->getQuery();
-//        return $query->getResult();
-//
-//    }
+    /**
+     * @Route("admin/user/{id}", name="admin_user")
+     */
+    public function index(int $id, Request $request, EntityManagerInterface $entityManager, UserRepository $userRepository): Response
+    {
+        $user = $userRepository->find($id);
+
+        return $this->render('admin/adminUserProfil.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    /**
+     * @Route("admin/user/{id}/modifier", name="admin_user_modifier")
+     */
+    public function modifier(int $id, Request $request, EntityManagerInterface $entityManager, UserRepository $userRepository, UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+
+        $user = $userRepository->find($id);
+
+        $userForm = $this->createForm(UserFormType::class, $user);
+        $userForm = $userForm->handleRequest($request);
+
+        if ($userForm->isSubmitted() && $userForm->isValid()){
+
+            // on récupère les images de l'user
+            if ($userForm->get('image')->getData()) {
+                if ($user->getUrlImage()) {
+                    unlink($this->getParameter('image_user_directory') . '/' . $user->getUrlImage());
+                }
+
+                $image = $userForm->get('image')->getData();
+                $urlImage = md5(uniqid()) . '.' . $image->guessExtension();
+                $image->move($this->getParameter('image_user_directory'), $urlImage);
+                $user->setUrlImage($urlImage);
+            }
+
+            // encode the password
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $userForm->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Le profil a été modifié');
+
+            return $this->redirectToRoute('gestionUsers');
+        }
+
+        return $this->render('user/modifier.html.twig', [
+            'userForm' => $userForm->createView()
+        ]);
+    }
+
+    /**
+     * Rendre un utilisateur inactif
+     * @Route("admin/user/{id}/disable", name="admin_user_disable")
+     */
+    public function disable(int $id, UserRepository $userRepository) {
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $user = $userRepository->find($id);
+        $user->setActif(0);
+        $entityManager->flush();
+
+        $this->addFlash('message', 'Utilisateur désactivé avec succès');
+        return $this->redirectToRoute('gestionUsers');
+
+    }
+
+    /**
+     * Suppression d'un utilisateur en BDD
+     * @Route("admin/user/{id}/delete", name="admin_user_delete")
+     */
+    public function delete(int $id, UserRepository $userRepository):Response {
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $user = $userRepository->find($id);
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        $this->addFlash('message', 'Utilisateur supprimé avec succès');
+        return $this->redirectToRoute('gestionUsers');
+
+    }
+
+
 }
